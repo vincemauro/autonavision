bee7c3500d2981bd7ad7dd7e01682a3ad048e2f7
vincemauro <vincenzo.mau@tiscali.it>
2019-05-31 18:03:20 +0200

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,97 +1,127 @@
 from splinter import Browser
 import time,sys
+import openpyxl as xl
 
 def overFillById(browser,id,fillString):
-    if browser.is_element_present_by_id(id,wait_time=2):
-        browser.find_by_id(id).fill(fillString)
-    return;
+	if browser.is_element_present_by_id(id,wait_time=2):
+		browser.find_by_id(id).fill(fillString)
+	return;
 
 def overClickById(browser,id):
-    if browser.is_element_present_by_id(id,wait_time=2):
-        browser.find_by_id(id).click() 
-    return;
+	if browser.is_element_present_by_id(id,wait_time=2):
+		browser.find_by_id(id).click() 
+	return;
 
 def overClickByText(browser,text):
-    if browser.is_element_present_by_text(text,wait_time=2):
-        browser.find_by_text(text).click() 
-    return;
+	if browser.is_element_present_by_text(text,wait_time=2):
+		browser.find_by_text(text).click() 
+	return;
+	
+def getStrTiming(hStr):
+	vTiming = ['', '']
+	if float(hStr) > 9:
+		return vTiming
+		
+	if float(hStr) % 0.5 > 0:
+		return vTiming
+		
+	integer = int(hStr)
+	decimal = float(hStr) % 1
+	
+	if integer > 0 or decimal > 0:
+		vTiming[0] = '00:00'
+		vTiming[1] = '0' + str(integer) + ':' + str(int(decimal * 60)).zfill(2)
+		
+	return vTiming;
 
-def insertNavRecord(browser,date,codiceCommessa,faseCommessa,hour ):
-    time.sleep(1)
-    overClickById(browser,'listaRigheOdT_btnNew')
-    time.sleep(1)
-    overClickById(browser,'btnRicercaCommessa')
-    time.sleep(2)
-    overFillById(browser,'searchCodCommessa',codiceCommessa)
-    overFillById(browser,'searchFase',faseCommessa)
-    browser.find_by_id('listaCommesse_btnFiltra').click()
-    time.sleep(1)
-    overClickByText(browser,codiceCommessa)
-    time.sleep(2)
-    overFillById(browser,'Data',date)
-    overClickById(browser,'Note')
-    overFillById(browser,'hord',hour)
-    time.sleep(1)
-    overClickById(browser,'btnSalva')
-    time.sleep(2)
-    return;
+def insertNavRecord(browser,date,codiceCommessa,faseCommessa,hBase,hStr ):
+	vTiming = getStrTiming(hStr)
+	time.sleep(1)
+	overClickById(browser,'listaRigheOdT_btnNew')
+	time.sleep(1)
+	overClickById(browser,'btnRicercaCommessa')
+	time.sleep(2)
+	overFillById(browser,'searchCodCommessa',codiceCommessa)
+	overFillById(browser,'searchFase',faseCommessa)
+	browser.find_by_id('listaCommesse_btnFiltra').click()
+	time.sleep(1)
+	overClickByText(browser,codiceCommessa)
+	time.sleep(2)
+	overFillById(browser,'Data',date)
+	overClickById(browser,'Note')
+	overFillById(browser,'hord',hBase)
+	time.sleep(1)
+	overFillById(browser,'tstraini',vTiming[0])
+	time.sleep(1)
+	overFillById(browser,'tstrafin',vTiming[1])
+	time.sleep(1)	
+	overClickById(browser,'btnSalva')
+	time.sleep(2)
+	return;
 
 def navisionLogin(browser):
-    browser.visit('https://navisionweb.lutech.it')
-    browser.find_by_id('UserName').fill(user)
-    browser.find_by_id('Password').fill(password)
-    browser.find_by_id('btnLogin').click()
-    return;
+	browser.visit('https://navisionweb.lutech.it')
+	browser.find_by_id('UserName').fill(sys.argv[1])
+	browser.find_by_id('Password').fill(sys.argv[2])
+	browser.find_by_id('btnLogin').click()
+	return;
 
 def newReport(browser):
-    browser.find_by_id('menu1').click()
-    time.sleep(1)
-    if browser.is_element_present_by_id('listaOdT_btnNew',wait_time=2):
-        browser.find_by_id('listaOdT_btnNew').click()
-    time.sleep(1)
-    return;  
+	browser.find_by_id('menu1').click()
+	time.sleep(1)
+	if browser.is_element_present_by_id('listaOdT_btnNew',wait_time=2):
+		browser.find_by_id('listaOdT_btnNew').click()
+	time.sleep(1)
+	return;  
 
 def openBozzaReport(browser):
-    browser.find_by_id('menu1').click()
-    browser.find_by_text('BOZZA').click()
-    time.sleep(1)
-    if browser.is_element_present_by_id('listaOdT_btnEdit',wait_time=2):
-        browser.find_by_id('listaOdT_btnEdit').click()
-    time.sleep(1)
-    return;  
-
-
-scriptName, user, password, csv = sys.argv
-
+	browser.find_by_id('menu1').click()
+	browser.find_by_text('BOZZA').click()
+	time.sleep(1)
+	if browser.is_element_present_by_id('listaOdT_btnEdit',wait_time=2):
+		browser.find_by_id('listaOdT_btnEdit').click()
+	time.sleep(1)
+	return;
+	
+if not sys.argv[1] or not sys.argv[2]:
+	exit()
+	
 browser = Browser('chrome')
 navisionLogin(browser)
 newReport(browser)
-#openBozzaReport(browser)
 
-f=open(csv, "r")
-fl =f.readlines()
-records = list()
-listaDate = list()
-for x in fl:
-    #print (x)
-    y = x.split(";")
-    if y[0]!='IGNORE':
-        if y[0]=='DATE':
-            for k in y:
-                if k != '' and k != '\n' and k!= 'DATE' and k!='END':
-                    listaDate.append(k)
-        if y[0]=='COMMESSA':
-            for j in range(4,y.index('END\n')):
-                record = list()
-                record.append(listaDate[j-4])
-                record.append(y[2])
-                record.append(y[3])
-                record.append(y[j])
-                records.append(record)
+wb = xl.load_workbook(filename = 'Navision.xlsm')
+ws = wb.active
+i = 4
+auxDate = ws.cell(row=2, column=i).value
 
-for p in records:
-    if p[0]!='' and p[1]!='' and p[2]!='' and p[3]!='':
-        insertNavRecord(browser,p[0],p[1],p[2],p[3])
+while auxDate:
+	print('Loading ' + auxDate)
+	j = 3
+	auxProgram = ws.cell(row=j, column=2).value
+	auxPhase = ws.cell(row=j, column=3).value
+	
+	while auxProgram:
+		#print(auxProgram + ':' + auxPhase)
+		auxHours = ws.cell(row=j, column=i).value
+		if auxHours:
+			#print(auxHours)
+			auxHours = auxHours.split('+')		
+			if len(auxHours) == 1:
+				auxHours.append('0')
+				
+			if not auxHours[1]:
+				auxHours[1] = '0'
+				
+			if float(auxHours[0]) + float(auxHours[1]) > 0:
+				#print(auxHours[0] + ' ' + auxHours[1])
+				insertNavRecord(browser,auxDate,auxProgram,auxPhase,auxHours[0],auxHours[1])
+		j += 1
+		auxProgram = ws.cell(row=j, column=2).value
+		auxPhase = ws.cell(row=j, column=3).value
+		
+	i += 1
+	auxDate = ws.cell(row=2, column=i).value
 
-        
-
+browser.quit()
+print('Load complete')
